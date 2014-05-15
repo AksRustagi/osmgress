ce16ad5fc858312329584b5b7670320efa292dad
Christoph Brill <egore911@gmail.com>
2014-05-15 21:25:28 +0200

    Move obtaining the connection to a servlet filter
    Closing will be done by the filter for us

--- src/main/java/de/egore911/osmgress/service/QueryServlet.java
+++ src/main/java/de/egore911/osmgress/service/QueryServlet.java
@@ -3,7 +3,6 @@ package de.egore911.osmgress.service;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,19 +20,12 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonWriter;
 
+import de.egore911.osmgress.http.ConnectionFilter;
+
 public class QueryServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6670846638516835427L;
-
-	@Override
-	public void init() throws ServletException {
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new ServletException(e);
-		}
-	}
-
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -46,11 +38,9 @@ public class QueryServlet extends HttpServlet {
 		double lon_max = Double.parseDouble(req.getParameter("lon_max"));
 
 		try {
-			Connection connection = DriverManager.getConnection(
-					"jdbc:postgresql://localhost/osmgress_mapnik", "osmgress",
-					"osmgress");
+			Connection connection = ConnectionFilter.getConnection();
 
-			PreparedStatement statement = connection
+			try (PreparedStatement statement = connection
 					.prepareStatement("SELECT osm_id, "
 							+ "name, "
 							+ "ST_Y(ST_Transform(way, 4326)) as latitude, "
@@ -70,31 +60,28 @@ public class QueryServlet extends HttpServlet {
 							+ "AND ST_YMax(ST_Transform(way, 4326)) > ? "
 							+ "AND ST_YMin(ST_Transform(way, 4326)) < ? "
 							+ "AND ST_XMax(ST_Transform(way, 4326)) > ? "
-							+ "AND ST_XMin(ST_Transform(way, 4326)) < ?");
+							+ "AND ST_XMin(ST_Transform(way, 4326)) < ?")) {
 
-			statement.setDouble(1, lat_min);
-			statement.setDouble(2, lat_max);
-			statement.setDouble(3, lon_min);
-			statement.setDouble(4, lon_max);
-			statement.setDouble(5, lat_min);
-			statement.setDouble(6, lat_max);
-			statement.setDouble(7, lon_min);
-			statement.setDouble(8, lon_max);
+				statement.setDouble(1, lat_min);
+				statement.setDouble(2, lat_max);
+				statement.setDouble(3, lon_min);
+				statement.setDouble(4, lon_max);
+				statement.setDouble(5, lat_min);
+				statement.setDouble(6, lat_max);
+				statement.setDouble(7, lon_min);
+				statement.setDouble(8, lon_max);
 
-			ResultSet resultSet = statement.executeQuery();
-
-			JsonArray jsonArray = new JsonArray();
-
-			while (resultSet.next()) {
-				jsonArray.add(convertToJson(resultSet));
+				JsonArray jsonArray = new JsonArray();
+				try (ResultSet resultSet = statement.executeQuery()) {
+					while (resultSet.next()) {
+						jsonArray.add(convertToJson(resultSet));
+					}
+				}
+				PrintWriter writer = resp.getWriter();
+				JsonWriter jsonWriter = new JsonWriter(writer);
+				jsonWriter.setLenient(true);
+				Streams.write(jsonArray, jsonWriter);
 			}
-			PrintWriter writer = resp.getWriter();
-			JsonWriter jsonWriter = new JsonWriter(writer);
-			jsonWriter.setLenient(true);
-			Streams.write(jsonArray, jsonWriter);
-
-			connection.close();
-
 		} catch (SQLException e) {
 			throw new ServletException(e);
 		}
@@ -107,7 +94,8 @@ public class QueryServlet extends HttpServlet {
 		long id = resultSetPoint.getLong(1);
 		jsonObject.add("id", new JsonPrimitive(id));
 		String name = resultSetPoint.getString(2);
-		jsonObject.add("name", name == null ? JsonNull.INSTANCE : new JsonPrimitive(name));
+		jsonObject.add("name", name == null ? JsonNull.INSTANCE
+				: new JsonPrimitive(name));
 		double latitude = resultSetPoint.getDouble(3);
 		jsonObject.add("latitude", new JsonPrimitive(latitude));
 		double longitude = resultSetPoint.getDouble(4);
