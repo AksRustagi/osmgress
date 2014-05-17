6ed67ab81f3aa1cd904ee2912b94a0548ead52ef
Christoph Brill <egore911@gmail.com>
2014-05-17 16:29:04 +0200

    Add missing file

---
+++ src/main/webapp/coloring.js
@@ -0,0 +1,42 @@
+var blue8 = L.icon({
+	iconUrl : 'images/blue8_32.png',
+	iconRetinaUrl : 'images/blue8_64.png',
+	iconSize : [ 32, 32 ],
+	iconAnchor : [ 16, 16 ],
+	popupAnchor : [ 0, -20 ]
+});
+var green8 = L.icon({
+	iconUrl : 'images/green8_32.png',
+	iconRetinaUrl : 'images/green8_64.png',
+	iconSize : [ 32, 32 ],
+	iconAnchor : [ 16, 16 ],
+	popupAnchor : [ 0, -20 ]
+});
+var gray8 = L.icon({
+	iconUrl : 'images/gray8_32.png',
+	iconRetinaUrl : 'images/gray8_64.png',
+	iconSize : [ 32, 32 ],
+	iconAnchor : [ 16, 16 ],
+	popupAnchor : [ 0, -20 ]
+});
+
+function toIcon(faction) {
+	if (faction == 'blue') {
+		return blue8;
+	} else if (faction == 'green') {
+		return green8;
+	} else {
+		return gray8;
+	}
+}
+
+function toColor(faction) {
+	if (faction == 'blue') {
+		return '#269eff';
+	} else if (faction == 'green') {
+		return '#3bea5e';
+	} else {
+		return 'white';
+	}
+}
+
